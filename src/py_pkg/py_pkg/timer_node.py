791eb80a3830e4ceec78ab6f98b83692ffd62c2a
mohab <mohabtantawy78@gmail.com>
2025-09-10 02:48:34 +0300

    timer_node done

---
+++ src/py_pkg/py_pkg/timer_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+class timer_node(Node):
+    def __init__(self):
+        super().__init__('timer_node')
+        self.publisher_ = self.create_publisher(Int16, 'topic', 10)
+        self.timer = self.create_timer(1.0, self.timer_callback)
+        self.count = 10
+
+    def timer_callback(self):
+        
+        while self.count != 0:
+            msg = Int16()
+            msg.data = f'{self.count}'
+            self.publisher_.publish(msg)
+            self.get_logger().info(f'Publishing: "{msg.data}"')
+            self.count -= 1
+        self.get_logger().info('Countdown finished!')
+        
+def main():
+    rclpy.init()
+    node = timer_node()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()

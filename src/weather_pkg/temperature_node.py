3bd3641ef0c7eb506a38ffef13b9e1ca2f89a43f
mohab <mohabtantawy78@gmail.com>
2025-09-10 15:41:47 +0300

    UPDATED

--- src/weather_pkg/temperature_node.py
+++ src/weather_pkg/temperature_node.py
@@ -7,15 +7,15 @@ from sensor_msgs.msg import Temperature
 
 class temperature_node(Node):
     def __init__(self):
-        super().__init__('temprature_node')
+        super().__init__('temperature_node')
         self.publisher_ = self.create_publisher(Temperature, '/temperature', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
-        self.get.logger().info('Temperature node has been started.')
+        self.get_logger().info('Temperature node has been started.')
 
     def timer_callback(self):
 
         msg = Temperature()
-        msg.temperature = random.randint(15, 40)  # Simulated temperature value between 15 and 40
+        msg.temperature = random.randint(15.0, 40.0)  # Simulated temperature value between 15 and 40
         msg.variance = 0.5  
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing temperature : {msg.temperature:.2f}°C (variance: {msg.variance})')

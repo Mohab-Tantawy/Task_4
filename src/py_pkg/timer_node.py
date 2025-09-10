557b0ac3255f1dba1f73a6249e8281082b537c3a
mohab <mohabtantawy78@gmail.com>
2025-09-10 16:03:01 +0300

    py_pkg UPDATED

--- src/py_pkg/timer_node.py
+++ src/py_pkg/timer_node.py
@@ -10,17 +10,18 @@ class timer_node(Node):
         self.publisher_ = self.create_publisher(Int16, 'topic', 10)
         self.timer = self.create_timer(1.0, self.timer_callback)
         self.count = 10
+        self.get_logger().info('Timer node has been started.')
 
     def timer_callback(self):
         
         
         msg = Int16()
-        msg.data = f'{self.count}'
+        msg.data = self.count
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing: "{msg.data}"')
         self.count -= 1
         if self.count < 0:
-            self.get_logger().info('Countdown finished!')
+            self.get_logger().info('Time is UP!')
             self.timer.cancel()
         
 def main():

a3ce857c0689c038b6394680bc80af1ff853769b
mohab <mohabtantawy78@gmail.com>
2025-09-10 04:33:44 +0300

    setup UPDATED

--- src/weather_pkg/setup.py
+++ src/weather_pkg/setup.py
@@ -20,10 +20,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'temperature_node = turtle_pkg.temperature_node:main',
-            'humidity_node = turtle_pkg.humidity_node:main',
-            'pressure_node = turtle_pkg.pressure_node:main',
-            'monitor_node = turtle_pkg.monitor_node:main',
+            'temperature_node = weather_pkg.temperature_node:main',
+            'humidity_node = weather_pkg.humidity_node:main',
+            'pressure_node = weather_pkg.pressure_node:main',
+            'monitor_node = weather_pkg.monitor_node:main',
         ],
     },
 )
